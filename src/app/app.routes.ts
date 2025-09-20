a511de48779a73e2edd51ec7b1b90a0f6de49a27
Ahmed Fawzy <ahmed.fawzy.cs1@gmail.com>
2025-09-20 21:04:42 +0300

    fix: added getPrerenderParams to handle dynamic routes for prerendering

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,29 +8,78 @@ import { authGuard } from './core/guards/auth-guard';
 import { isLoggedGuard } from './core/guards/is-logged-guard';
 
 export const routes: Routes = [
-    {path:'',redirectTo:'home' , pathMatch:'full'},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
 
-    {path:'',component:AuthLayoutComponent,canActivate:[isLoggedGuard],
-        children:[
-        {path:'login' ,loadComponent:()=>import('./core/auth/login/login.component').then((c)=>c.LoginComponent) , title:"Login Page"},
-        {path:'register' ,loadComponent:()=>import('./core/auth/register/register.component').then((c)=>c.RegisterComponent) , title:"Register Page"},
-        {path:'forget' ,loadComponent:()=>import('./core/auth/forget-password/forget-password.component').then((c)=>c.ForgetPasswordComponent) , title:"ForgetPassword Page"},
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    canActivate: [isLoggedGuard],
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./core/auth/login/login.component').then((c) => c.LoginComponent),
+        title: 'Login Page',
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./core/auth/register/register.component').then((c) => c.RegisterComponent),
+        title: 'Register Page',
+      },
+      {
+        path: 'forget',
+        loadComponent: () =>
+          import('./core/auth/forget-password/forget-password.component').then(
+            (c) => c.ForgetPasswordComponent
+          ),
+        title: 'ForgetPassword Page',
+      },
+    ],
+  },
 
-    ],},
-
-    {path:'',component:BlankLayoutComponent, canActivate:[authGuard], 
-        children:[
-        { path:'home', loadComponent:()=>import('./features/home/home.component').then((c)=>c.HomeComponent),title:"Home Page" },
-        { path:'cart',  loadComponent:()=>import('./features/cart/cart.component').then((c)=>c.CartComponent),title:"Cart Page" },
-        { path:'products', loadComponent:()=>import('./features/products/products.component').then((c)=>c.ProductsComponent),title:"Products Page" },
-        { path:'brands',  loadComponent:()=>import('./features/brands/brands.component').then((c)=>c.BrandsComponent),title:"Brands Page" },
-        { path:'categories', loadComponent:()=>import('./features/categories/categories.component').then((c)=>c.CategoriesComponent),title:"Categories Page" },
-        { path:'allorders',  loadComponent:()=>import('./features/allorders/allorders.component').then((c)=>c.AllordersComponent),title:"allOrders Page" },
-        { path:'details/:id', component:DetailsComponent,title:"Details Page" },
-        { path:'checkout/:id', component:CheckoutComponent,title:"Checkout Page" },
-        
-        
-    ],},
-     {path:'**' , component:NotfoundComponent, title:"Notfound Page"}
-   
+  {
+    path: '',
+    component: BlankLayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./features/home/home.component').then((c) => c.HomeComponent),
+        title: 'Home Page',
+      },
+      {
+        path: 'cart',
+        loadComponent: () => import('./features/cart/cart.component').then((c) => c.CartComponent),
+        title: 'Cart Page',
+      },
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('./features/products/products.component').then((c) => c.ProductsComponent),
+        title: 'Products Page',
+      },
+      {
+        path: 'brands',
+        loadComponent: () =>
+          import('./features/brands/brands.component').then((c) => c.BrandsComponent),
+        title: 'Brands Page',
+      },
+      {
+        path: 'categories',
+        loadComponent: () =>
+          import('./features/categories/categories.component').then((c) => c.CategoriesComponent),
+        title: 'Categories Page',
+      },
+      {
+        path: 'allorders',
+        loadComponent: () =>
+          import('./features/allorders/allorders.component').then((c) => c.AllordersComponent),
+        title: 'allOrders Page',
+      },
+      { path: 'details/:id', component: DetailsComponent, title: 'Details Page' },
+      { path: 'checkout/:id', component: CheckoutComponent, title: 'Checkout Page' },
+    ],
+  },
+  { path: '**', component: NotfoundComponent, title: 'Notfound Page' },
 ];
